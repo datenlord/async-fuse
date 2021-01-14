2f10f30e7042defae6a7e188c7e43dab831a8f02
Nugine <nugine@foxmail.com>
2021-01-14 21:15:28 +0800

    epoll

--- async-fuse/src/server/fuse_io.rs
+++ async-fuse/src/server/fuse_io.rs
@@ -2,30 +2,34 @@
 
 use crate::core::{FuseDesc, FuseWrite};
 
-use std::io::{self, Read, Write};
+use std::io::{self, Read};
 use std::pin::Pin;
 use std::sync::Arc;
 use std::task::{Context, Poll};
 
+use async_io::Async;
 use blocking::unblock;
+use futures_util::io::AsyncWrite;
+use futures_util::ready;
 
 /// The writer of a FUSE connection
 #[derive(Debug, Clone)]
 pub struct ConnWriter {
     /// Arc fd
-    fd: Arc<FuseDesc>,
+    fd: Arc<Async<FuseDesc>>,
 }
 
 impl FuseWrite for ConnWriter {
     fn poll_reply(
         self: Pin<&mut Self>,
-        _: &mut Context<'_>,
+        cx: &mut Context<'_>,
         bufs: &[io::IoSlice<'_>],
     ) -> Poll<io::Result<()>> {
         // libc::writev is atomic, so we don't need to lock the fd.
         // fuse fd is always writable.
         let len: usize = bufs.iter().map(|s| s.len()).sum();
-        let nwrite = (&*self.fd).write_vectored(bufs)?;
+        let mut fd = &*self.fd;
+        let nwrite = ready!(Pin::new(&mut fd).poll_write_vectored(cx, bufs)?);
         assert_eq!(len, nwrite);
         Poll::Ready(Ok(()))
     }
@@ -35,13 +39,13 @@ impl FuseWrite for ConnWriter {
 #[derive(Debug)]
 pub struct ConnReader {
     /// Arc fd
-    fd: Arc<FuseDesc>,
+    fd: Arc<Async<FuseDesc>>,
 }
 
 impl ConnReader {
     /// Gets the underlying fd
     pub fn get_fd(&self) -> &FuseDesc {
-        &*self.fd
+        self.fd.get_ref()
     }
 
     /// Reads a request into the buffer, using a thread pool.
@@ -49,14 +53,14 @@ impl ConnReader {
     where
         B: AsMut<[u8]> + Send + 'static,
     {
-        let fd = Arc::clone(&self.fd);
-        unblock(move || (&*fd).read(buf.as_mut()).map(|nread| (buf, nread))).await
+        let nread = self.fd.read_with(|fd| (&*fd).read(buf.as_mut())).await?;
+        Ok((buf, nread))
     }
 }
 
 /// Connects to `/dev/fuse`
 pub async fn connect() -> io::Result<(ConnReader, ConnWriter)> {
-    let fd = Arc::new(unblock(FuseDesc::open).await?);
+    let fd = Arc::new(Async::new(unblock(FuseDesc::open).await?)?);
 
     let writer = ConnWriter { fd };
 
